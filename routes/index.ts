6c2b94e9dec02399c01d497cae625f7fff2b4e14
shadreza <shadreza100@gmail.com>
2023-07-26 01:30:30 +0600

    converted the backend system to TS. added cors. added prisma and made connection with the postgresql database hosted in railway.

---
+++ routes/index.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+
+const router = Router()
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const getUsersData = async () => {
+    const users = await prisma.user.findMany()
+    return users
+}
+
+router.get('/users', async (req, res) => {
+    await getUsersData()
+        .then(async (data) => {
+        res.send(data)
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        res.sendStatus(500)
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+})
+
+router.get('/', (req, res) => {
+    res.send("Assalamu Alaikum")
+})
+
+// router.get('/', async (req, res) => {
+//     const users = await prisma.user.findMany()
+//     res.json(users)
+// })
+
+
+export default router
