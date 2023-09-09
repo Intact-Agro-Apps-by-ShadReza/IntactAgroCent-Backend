8593edc57f839e9ef5953900ce25b4e7514c057d
shadreza <shadreza100@gmail.com>
2023-09-09 17:56:54 +0600

    adding additional updationTime to some tables

---
+++ prisma/migrations/20230909113521_adding_updation_time/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "updationTime" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "Tag" ADD COLUMN     "updationTime" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "updationTime" TIMESTAMP(3);
